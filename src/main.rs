f156aa5bc6a8783b2977c341f6d80b8ebf2cc1f6
SuanCaiYv <jokerblack2508826394@gmail.com>
2024-03-31 17:29:18 +0800

    add some startup flags.

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,22 @@ use std::env;
 
 use net::server::{Server, ServerConfig};
 use tracing::Level;
+use structopt::StructOpt;
 
 mod db;
 mod net;
 
+#[derive(StructOpt, Debug)]
+#[structopt(name = "rustatic")]
+struct Opt {
+    #[structopt(short = "r", long, default_value = "")]
+    /// the file root folder
+    root: String,
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    let opt = Opt::from_args();
     tracing_subscriber::fmt()
         .event_format(
             tracing_subscriber::fmt::format()
@@ -23,10 +33,15 @@ async fn main() -> anyhow::Result<()> {
         data_port: 8191,
         cert: rustls::Certificate(include_bytes!("../tls/dev-server.crt.der").to_vec()),
         key: rustls::PrivateKey(include_bytes!("../tls/dev-server.key.der").to_vec()),
-        root_folder: format!(
-            "{}/rustatic/data",
-            env::var("HOME").expect("HOME env var not set")
-        ),
+        root_folder: if opt.root.len() == 0 {
+            format!(
+                "{}/rustatic/data",
+                env::var("HOME").expect("HOME env var not set")
+            )
+        } else {
+            opt.root.clone()
+        },
     };
+    println!("{}", server_config.root_folder);
     Server::new(server_config).run().await
 }
